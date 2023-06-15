2d2ae4902c459b90d46998a9c708e8d46ab73220
maxivega0 <maximilianovega600@gmail.com>
2023-06-15 12:56:10 -0300

    llamada al api funcionanado

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -2,17 +2,49 @@ import { useState, useEffect } from "react";
 import { Container, Form, Button } from "react-bootstrap";
 
 const Formulario = () => {
-    const [categoria, setCategoria] = useState("")
-    const [noticias, setNoticias] = useState([])
+  const [categoria, setCategoria] = useState("");
+  const [noticias, setNoticias] = useState([]);
+  const [mostrarNoticias, setMostrarNoticias] = useState(false);
+  useEffect(() => {
+    // consultarAPI();
+  }, []);
 
+  const consultarAPI = async () => {
+    try {
+      const respuesta = await fetch(
+        `https://newsdata.io/api/1/news?apikey=pub_24226bea9b87e0d87603664a9762b89a71aa4&category=${categoria}`
+      );
+      const dato = await respuesta.json();
+      console.log(respuesta);
+      // console.log(respuesta.ok);
+      console.log(dato);
+      setNoticias(dato);
+      setMostrarNoticias(true);
+      console.log(noticias);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setCategoria(e.target.value);
+    // setPais(e.target.value);
+    console.log(categoria);
+    // console.log(ciudad);
+    consultarAPI();
+  };
 
-    return (
-        <>
-            
-      <Form className="row">
-        <Form.Group className="mb-3 d-flex flex-column" controlId="tarea">
-        <Form.Label>Ingrese una categoria de noticias:</Form.Label>
-          <Form.Select>
+  return (
+    <><Container className="text-center">
+
+   
+      <Form className="row" onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="tarea">
+          <Form.Label>Ingrese una categoria de noticias:</Form.Label>
+          <Form.Select
+            onChange={(e) => setCategoria(e.target.value)}
+            value={categoria}
+          >
             <option>Elige que tipo de noticia te interesa</option>
             <option value="business">Negocios</option>
             <option value="entertainment">Entretenimiento</option>
@@ -26,18 +58,18 @@ const Formulario = () => {
             <Button
               variant="warning"
               type="submit"
-            //   onClick={() => setCategoriaSeleccionada(categoria)}
             >
               Buscar
             </Button>
           </Container>
         </Form.Group>
       </Form>
+      </Container>
       <Container className="row">
         {/* {mostrarNoticias && <ListaNoticias noticias={noticias.results} />} */}
       </Container>
-        </>
-    );
+    </>
+  );
 };
 
 export default Formulario;
